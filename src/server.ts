159390a7b29d6ba08575a814aeaa1256064d3c52
Diego Prestes <diegoprestesgit@hotmail.com>
2020-04-24 20:26:25 -0300

    fazendo funcionar com eslint

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
 import express from 'express'
+import routes from './routes/index'
 
 const app = express()
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'restartando' })
-})
+app.use(routes)
 
 app.listen(3333, () => {
   console.log('all right')
